a29ed890d4e3be641b7297ed19598b08589e1c29
Princess Nkabinde <52870123+Princessn@users.noreply.github.com>
2022-05-31 16:31:59 +0200

    update on the app

--- apple-price-planner.js
+++ apple-price-planner.js
@@ -1,1 +1,59 @@
 
+var boxCost = 0
+var numOfApples = 0
+var numOfApplesInPacket = 0
+var requiredProfit = 0
+
+  var costPerApple = 0;
+  var costPerPacket = 0;
+  var numOfPackets = 0;
+  var packetPrice = 0;
+
+  function setBoxCost(cost) {
+    if (price == Number(price)) {
+        boxCost = Number(price);
+      }
+    }
+  return boxCost;
+
+  function numOfApplesInBox(qty) {
+    
+    if (noOfApples == Number(noOfApples)) {
+        numOfApples = Number(noOfApples);
+    }
+    return numOfApples 
+  }
+
+  function setPercentProfit(profit) {
+    
+
+    if (profit == Number(profit)) {
+        requiredProfit = Number(profit);
+    }
+    return requiredProfit
+  }
+
+  function costOfEachApple() {
+    if (boxCost && numOfApples) {
+        costPerApple  = boxCost/ numOfApples;
+    }
+    
+    return "R" + costOfEachApple.toFixed(2);
+  }
+
+  function numberOfPackets() {
+    if (numOfApples && numberPerPacket) {
+        numOfPackets =numOfApples / numOfApplesInPacket;
+    }
+    return numOfPackets;
+}
+
+return {
+     setBoxCost,
+    costOfEachApple,
+    numberOfPackets,
+    setPercentProfit,
+    numOfApplesInBox,
+    
+}
+        
